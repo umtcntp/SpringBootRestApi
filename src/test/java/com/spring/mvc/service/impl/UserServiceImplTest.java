50674f653543866788f8b3dd485a555c5275b2c9
Umut Can Top <umutcantop@mail.com>
2022-08-16 17:31:39 +0300

    Test class has been improved

--- src/test/java/com/spring/mvc/service/impl/UserServiceImplTest.java
+++ src/test/java/com/spring/mvc/service/impl/UserServiceImplTest.java
@@ -1,12 +1,17 @@
 package com.spring.mvc.service.impl;
 
-import org.junit.Before;
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.spring.mvc.dto.UserDto;
+import com.spring.mvc.entity.User;
 import com.spring.mvc.repository.UserRepository;
-import com.spring.mvc.service.impl.UserServiceImpl;
 
 @SpringBootTest
 public class UserServiceImplTest {
@@ -15,18 +20,47 @@ public class UserServiceImplTest {
 	private UserRepository userRepository;
 	private ModelMapper modelMapper;
 	
-	@Before
+	@BeforeEach
 	public void setUp() throws Exception{
+		System.err.println("wedıowoı");
+		modelMapper = new ModelMapper();
 		userRepository = Mockito.mock(UserRepository.class);
 		modelMapper = Mockito.mock(ModelMapper.class);
-		userServiceImpl  = new UserServiceImpl(userRepository, modelMapper);
-		
+		userServiceImpl  = new UserServiceImpl(userRepository, modelMapper);		
 	}	
 	
 	@Test
-	public void whenCreateUserCalledWithValidRequest_ThenItShouldReturnValidUserDTO() {
-		System.err.println("wsgışewrjommgsvndfvdn");
+	public void whenCreateUserCalledWithValidRequest_ThenItShouldReturnValidUserDTO() {		
+		UserDto userDto = new UserDto();
+		userDto.setFirstName("Hakan");
+		userDto.setLastName("Karaagac");
+		
+
+		User user = modelMapper.map(userDto,User.class);	
+		user.setCreateDate(new Date());
+		user.setCreatedBy("Admin");
+		
+		Mockito.when(userServiceImpl.createUser(userDto)).thenReturn(userDto);
+		Mockito.when(userRepository.save(user)).thenReturn(user);
+		
+		UserDto result = userServiceImpl.createUser(userDto);
+		
+		Assert.assertEquals(result, userDto);
+		
+		Mockito.verify(userServiceImpl.createUser(userDto));
+		Mockito.verify(userRepository).save(user);
+		
 	}
+	
+	/*
+	@Test
+	public void whenCreateUserCalledWithrrValidRequest_ThenItShouldReturnValidUserDTO() {
+		String message = "umuttt";
+		UserDto userDto = new UserDto(message);
+		userDto.setFirstName("ddqdq");
+		userDto.setLastName("Karqdqwaagac");
+		Mockito.when(userServiceImpl.createUser(userDto)).thenReturn(userDto);
+	}*/
 	
 	
 	
